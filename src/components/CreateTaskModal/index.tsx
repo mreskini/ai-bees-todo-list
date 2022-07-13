3c60b433bc5ae36bfd55611395dfe3ea9e9e226d
Mohammad Eskini <mreskini30@gmail.com>
2022-07-13 18:51:52 +0430

    feat: add necessary elements for the create task modal

--- src/components/CreateTaskModal/index.tsx
+++ src/components/CreateTaskModal/index.tsx
@@ -1,4 +1,13 @@
-import { Box, Modal, Typography } from "@mui/material"
+import {
+    Box,
+    Button,
+    FormControlLabel,
+    Modal,
+    Radio,
+    RadioGroup,
+    TextField,
+    Typography,
+} from "@mui/material"
 import { FC } from "react"
 import styles from "./CreateTaskModal.module.scss"
 
@@ -17,13 +26,58 @@ const CreateTaskModal: FC<Props> = ({ open, handleClose }) => {
                 aria-describedby="This modal is used to create new tasks"
             >
                 <Box className={styles.modal}>
-                    <Typography
-                        id="modal-modal-title"
-                        variant="h6"
-                        component="h2"
-                    >
-                        Text in a modal
+                    <Typography className={styles.subject}>
+                        Create New Task
                     </Typography>
+                    <TextField
+                        label="Task title"
+                        className={styles.title}
+                        variant="outlined"
+                    />
+                    <TextField
+                        label="Task description"
+                        className={styles.description}
+                        rows={5}
+                        multiline
+                    />
+                    <TextField
+                        label="Gift and KPI for this task ;)"
+                        className={styles.targets}
+                        variant="outlined"
+                    />
+                    <RadioGroup
+                        row
+                        aria-labelledby="Task priority"
+                        className={styles.priority}
+                    >
+                        <FormControlLabel
+                            value="low"
+                            control={<Radio />}
+                            label="Low"
+                        />
+                        <FormControlLabel
+                            value="medium"
+                            control={<Radio />}
+                            label="Medium"
+                        />
+                        <FormControlLabel
+                            value="hight"
+                            control={<Radio />}
+                            label="High"
+                        />
+                    </RadioGroup>
+                    <Box className={styles.actions}>
+                        <Button
+                            variant="outlined"
+                            color="error"
+                            onClick={handleClose}
+                        >
+                            cancel
+                        </Button>
+                        <Button variant="contained" onClick={handleClose}>
+                            Add to Tasks
+                        </Button>
+                    </Box>
                 </Box>
             </Modal>
         </div>
