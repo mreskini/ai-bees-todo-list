28f438050f3cc62806af763df942fa40d789aef0
Mohammad Eskini <mreskini30@gmail.com>
2022-07-13 18:19:53 +0430

    style: update create new task modal ui

--- src/components/CreateTaskModal/index.tsx
+++ src/components/CreateTaskModal/index.tsx
@@ -1,4 +1,4 @@
-import { Modal } from "@mui/material"
+import { Box, Modal, Typography } from "@mui/material"
 import { FC } from "react"
 import styles from "./CreateTaskModal.module.scss"
 
@@ -16,7 +16,15 @@ const CreateTaskModal: FC<Props> = ({ open, handleClose }) => {
                 aria-labelledby="Create task modal"
                 aria-describedby="This modal is used to create new tasks"
             >
-                <div>Foobar is here</div>
+                <Box className={styles.modal}>
+                    <Typography
+                        id="modal-modal-title"
+                        variant="h6"
+                        component="h2"
+                    >
+                        Text in a modal
+                    </Typography>
+                </Box>
             </Modal>
         </div>
     )
