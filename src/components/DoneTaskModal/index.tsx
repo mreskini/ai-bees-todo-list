bfd1d6a610d1826ae92fd4cde6cd24ba7f08a2b9
Mohammad Eskini <mreskini30@gmail.com>
2022-07-14 16:24:15 +0430

    style: add style for task item in done tasks modal

--- src/components/DoneTaskModal/index.tsx
+++ src/components/DoneTaskModal/index.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react"
-import { Box, Modal } from "@mui/material"
+import { Box, Button, Grid, Modal, Paper } from "@mui/material"
 import styles from "./DoneTasksModal.module.scss"
 import { useApp } from "../../contexts/AppContext"
 
@@ -30,9 +30,61 @@ const DoneTasksModal: FC<Props> = ({ open, handleClose }) => {
                         {doneTasksList.length === 0 && (
                             <div>No Done Tasks Found</div>
                         )}
-                        {doneTasksList.map(task => {
-                            return <div key={task.token}>{task.title}</div>
-                        })}
+                        {doneTasksList.map(
+                            ({ token, title, description, priority }) => {
+                                return (
+                                    <Paper
+                                        variant="outlined"
+                                        elevation={1}
+                                        key={token}
+                                        className={styles.item}
+                                    >
+                                        <Grid
+                                            display="flex"
+                                            alignItems="center"
+                                            justifyContent="space-between"
+                                        >
+                                            <Grid>
+                                                <div className={styles.title}>
+                                                    {title}
+                                                </div>
+                                                <div
+                                                    className={
+                                                        styles.description
+                                                    }
+                                                >
+                                                    {description.slice(0, 30)}
+                                                    {description.length > 30 &&
+                                                        "..."}
+                                                </div>
+                                            </Grid>
+                                            <Grid>
+                                                <div
+                                                    className={styles.priority}
+                                                >
+                                                    <div>{priority}</div>
+                                                    <div
+                                                        className={`${
+                                                            styles.bullet
+                                                        } ${
+                                                            styles[
+                                                                priority ===
+                                                                "HIGH"
+                                                                    ? "bullet-high"
+                                                                    : priority ===
+                                                                      "MEDIUM"
+                                                                    ? "bullet-mid"
+                                                                    : "bullet-low"
+                                                            ]
+                                                        }`}
+                                                    ></div>
+                                                </div>
+                                            </Grid>
+                                        </Grid>
+                                    </Paper>
+                                )
+                            }
+                        )}
                     </div>
                 </Box>
             </Modal>
