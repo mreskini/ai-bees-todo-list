f1ace7036b54ef24fa9deeb5a8164a547c646bc0
Mohammad Eskini <mreskini30@gmail.com>
2022-07-14 14:30:46 +0430

    fix: solve empty task list logical issue

--- src/components/TasksList/index.tsx
+++ src/components/TasksList/index.tsx
@@ -9,11 +9,12 @@ import styles from "./TasksList.module.scss"
 const TasksList = () => {
     // States and Hooks
     const { tasksList, getTaskByToken, doneTaskByToken } = useApp()
-    const showTasksList = tasksList.length > 0
+    const openTasksList = tasksList.filter(task => task.status === "OPEN")
+    const showTasksList = openTasksList.length > 0
     const [isCreateModalOpen, setIsCreateModalOpen] = useState(false)
     const [isDetailsModalOpen, setIsDetailsModalOpen] = useState(false)
     const [currentTask, setCurrentTask] = useState<Task | undefined>(
-        tasksList[0]
+        openTasksList[0]
     )
 
     // Methods
@@ -46,107 +47,101 @@ const TasksList = () => {
             {showTasksList ? (
                 <Grid container alignItems="center" justifyContent="center">
                     <Grid xs={8}>
-                        {tasksList
-                            .filter(task => task.status === "OPEN")
-                            .map(
-                                (
-                                    { token, title, description, priority },
-                                    index
-                                ) => {
-                                    return (
-                                        <Button
-                                            key={index}
-                                            className={styles.item}
-                                            onClick={() =>
-                                                handleDetailsModalOpen(token)
-                                            }
+                        {openTasksList.map(
+                            (
+                                { token, title, description, priority },
+                                index
+                            ) => {
+                                return (
+                                    <Button
+                                        key={index}
+                                        className={styles.item}
+                                        onClick={() =>
+                                            handleDetailsModalOpen(token)
+                                        }
+                                    >
+                                        <Paper
+                                            variant="outlined"
+                                            elevation={1}
+                                            className={styles["item-inner"]}
                                         >
-                                            <Paper
-                                                variant="outlined"
-                                                elevation={1}
-                                                className={styles["item-inner"]}
+                                            <Grid
+                                                display="flex"
+                                                alignItems="center"
+                                                justifyContent="space-between"
                                             >
-                                                <Grid
-                                                    display="flex"
-                                                    alignItems="center"
-                                                    justifyContent="space-between"
-                                                >
-                                                    <Grid>
-                                                        <div
-                                                            className={
-                                                                styles.title
-                                                            }
-                                                        >
-                                                            {title}
-                                                        </div>
-                                                        <div
-                                                            className={
-                                                                styles.description
-                                                            }
-                                                        >
-                                                            {description.slice(
-                                                                0,
-                                                                30
-                                                            )}
-                                                            {description.length >
-                                                                30 && "..."}
-                                                        </div>
-                                                    </Grid>
-                                                    <Grid>
-                                                        <div
-                                                            className={
-                                                                styles.priority
-                                                            }
-                                                        >
-                                                            <div>
-                                                                {priority}
-                                                            </div>
-                                                            <div
-                                                                className={`${
-                                                                    styles.bullet
-                                                                } ${
-                                                                    styles[
-                                                                        priority ===
-                                                                        "HIGH"
-                                                                            ? "bullet-high"
-                                                                            : priority ===
-                                                                              "MEDIUM"
-                                                                            ? "bullet-mid"
-                                                                            : "bullet-low"
-                                                                    ]
-                                                                }`}
-                                                            ></div>
-                                                        </div>
-                                                        <div
-                                                            className={
-                                                                styles.actions
-                                                            }
-                                                        >
-                                                            <Button
-                                                                variant="contained"
-                                                                color="success"
-                                                                onClick={() =>
-                                                                    doneTaskByToken(
-                                                                        token
-                                                                    )
-                                                                }
-                                                            >
-                                                                Done Task
-                                                            </Button>
-                                                            <Button
-                                                                variant="contained"
-                                                                color="info"
-                                                            >
-                                                                Edit Task
-                                                            </Button>
-                                                        </div>
-                                                    </Grid>
+                                                <Grid>
+                                                    <div
+                                                        className={styles.title}
+                                                    >
+                                                        {title}
+                                                    </div>
+                                                    <div
+                                                        className={
+                                                            styles.description
+                                                        }
+                                                    >
+                                                        {description.slice(
+                                                            0,
+                                                            30
+                                                        )}
+                                                        {description.length >
+                                                            30 && "..."}
+                                                    </div>
                                                 </Grid>
-                                            </Paper>
-                                        </Button>
-                                    )
-                                }
-                            )}
+                                                <Grid>
+                                                    <div
+                                                        className={
+                                                            styles.priority
+                                                        }
+                                                    >
+                                                        <div>{priority}</div>
+                                                        <div
+                                                            className={`${
+                                                                styles.bullet
+                                                            } ${
+                                                                styles[
+                                                                    priority ===
+                                                                    "HIGH"
+                                                                        ? "bullet-high"
+                                                                        : priority ===
+                                                                          "MEDIUM"
+                                                                        ? "bullet-mid"
+                                                                        : "bullet-low"
+                                                                ]
+                                                            }`}
+                                                        ></div>
+                                                    </div>
+                                                    <div
+                                                        className={
+                                                            styles.actions
+                                                        }
+                                                    >
+                                                        <Button
+                                                            variant="contained"
+                                                            color="success"
+                                                            onClick={() =>
+                                                                doneTaskByToken(
+                                                                    token
+                                                                )
+                                                            }
+                                                        >
+                                                            Done Task
+                                                        </Button>
+                                                        <Button
+                                                            variant="contained"
+                                                            color="info"
+                                                        >
+                                                            Edit Task
+                                                        </Button>
+                                                    </div>
+                                                </Grid>
+                                            </Grid>
+                                        </Paper>
+                                    </Button>
+                                )
+                            }
+                        )}
                     </Grid>
                     <FloatingAddButton handleOpen={handleCreateModalOpen} />
                 </Grid>
