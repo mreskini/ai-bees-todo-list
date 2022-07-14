a8974d3027e01255be26babb155fbc608dca2509
Mohammad Eskini <mreskini30@gmail.com>
2022-07-14 23:54:09 +0430

    refactor: add create form component to improve reusbility

--- src/components/EditTaskModal/index.tsx
+++ src/components/EditTaskModal/index.tsx
@@ -1,17 +1,10 @@
-import {
-    Box,
-    Button,
-    FormControlLabel,
-    Modal,
-    Radio,
-    RadioGroup,
-    TextField,
-} from "@mui/material"
+import { Box, Modal } from "@mui/material"
 import { useEffect, useState } from "react"
 import { Priority, useTasks } from "../../contexts/TasksContext"
 import styles from "./EditTaskModal.module.scss"
 import modalStyles from "../../styles/modules/Modal.module.scss"
 import { useApp } from "../../contexts/AppContext"
+import TaskForm from "../TaskForm"
 
 const EditTaskModal = () => {
     // States and Hooks
@@ -21,45 +14,24 @@ const EditTaskModal = () => {
         handleEditModalClose: handleClose,
         currentTask: task,
     } = useApp()
-    const [titleValue, setTitleValue] = useState<string>("")
-    const [descriptionValue, setDescriptionValue] = useState<string>("")
-    const [targetsValue, setTargetsValue] = useState<string>("")
-    const [taskPriority, setTaskPriority] = useState<Priority>("LOW")
+
+    const [title, setTitle] = useState<string>("")
+    const [description, setDescription] = useState<string>("")
+    const [targets, setTargets] = useState<string>("")
+    const [priority, setPriority] = useState<Priority>("LOW")
 
     // Methods
-    const addToTasksButtonClicked = () => {
-        editTask(
-            task.token,
-            titleValue,
-            descriptionValue,
-            targetsValue,
-            taskPriority
-        )
+    const editTaskButtonClick = () => {
+        editTask(task.token, title, description, targets, priority)
         handleClose()
     }
 
     useEffect(() => {
-        setTitleValue(task.title)
-        setDescriptionValue(task.description)
-        setTargetsValue(task.targets)
-        setTaskPriority(task.priority)
+        setTitle(task.title)
+        setDescription(task.description)
+        setTargets(task.targets)
+        setPriority(task.priority)
     }, [task])
-
-    const getPriorityByNumber = (number: string) => {
-        if (number === "1") return "LOW"
-        if (number === "2") return "MEDIUM"
-        if (number === "3") return "HIGH"
-
-        return "LOW"
-    }
-
-    const getPriorityNumberByName = (name: string): string => {
-        if (name === "LOW") return "1"
-        if (name === "MEDIUM") return "2"
-        if (name === "HIGH") return "3"
-
-        return "1"
-    }
 
     // Render
     return (
@@ -72,68 +44,18 @@ const EditTaskModal = () => {
             >
                 <Box className={modalStyles.modal}>
                     <div className={modalStyles.subject}>Edit Task</div>
-                    <TextField
-                        label="Task title"
-                        className={styles.title}
-                        variant="outlined"
-                        value={titleValue}
-                        onChange={e => setTitleValue(e.target.value)}
+                    <TaskForm
+                        title={title}
+                        setTitle={setTitle}
+                        description={description}
+                        setDescription={setDescription}
+                        targets={targets}
+                        setTargets={setTargets}
+                        priority={priority}
+                        setPriority={setPriority}
+                        handleAction={editTaskButtonClick}
+                        handleClose={handleClose}
                     />
-                    <TextField
-                        label="Task description"
-                        className={styles.description}
-                        rows={5}
-                        multiline
-                        value={descriptionValue}
-                        onChange={e => setDescriptionValue(e.target.value)}
-                    />
-                    <TextField
-                        label="Gift and KPI for this task ;)"
-                        className={styles.targets}
-                        variant="outlined"
-                        value={targetsValue}
-                        onChange={e => setTargetsValue(e.target.value)}
-                    />
-                    <RadioGroup
-                        row
-                        aria-labelledby="Task priority"
-                        className={styles.priority}
-                        value={getPriorityNumberByName(taskPriority)}
-                        onChange={e =>
-                            setTaskPriority(getPriorityByNumber(e.target.value))
-                        }
-                    >
-                        <FormControlLabel
-                            value="1"
-                            control={<Radio />}
-                            label="Low"
-                        />
-                        <FormControlLabel
-                            value="2"
-                            control={<Radio />}
-                            label="Medium"
-                        />
-                        <FormControlLabel
-                            value="3"
-                            control={<Radio />}
-                            label="High"
-                        />
-                    </RadioGroup>
-                    <Box className={styles.actions}>
-                        <Button
-                            variant="outlined"
-                            color="error"
-                            onClick={handleClose}
-                        >
-                            cancel
-                        </Button>
-                        <Button
-                            variant="contained"
-                            onClick={addToTasksButtonClicked}
-                        >
-                            Edit Task
-                        </Button>
-                    </Box>
                 </Box>
             </Modal>
         </div>
---
+++ src/components/TaskForm/index.tsx
@@ -0,0 +1,107 @@
+import {
+    Box,
+    Button,
+    FormControlLabel,
+    Radio,
+    RadioGroup,
+    TextField,
+} from "@mui/material"
+import { FC } from "react"
+import { Priority } from "../../contexts/TasksContext"
+import styles from "./TaskForm.module.scss"
+
+type Props = {
+    title: string
+    setTitle(value: string): void
+    description: string
+    setDescription(value: string): void
+    targets: string
+    setTargets(value: string): void
+    priority: Priority
+    setPriority(value: Priority): void
+    handleAction(): void
+    handleClose(): void
+}
+
+const TaskForm: FC<Props> = ({
+    title,
+    setTitle,
+    description,
+    setDescription,
+    targets,
+    setTargets,
+    priority,
+    setPriority,
+    handleAction,
+    handleClose,
+}) => {
+    // Methods
+    const getPriorityByNumber = (number: string) => {
+        if (number === "1") return "LOW"
+        if (number === "2") return "MEDIUM"
+        if (number === "3") return "HIGH"
+
+        return "LOW"
+    }
+
+    const getPriorityNumberByName = (name: string): string => {
+        if (name === "LOW") return "1"
+        if (name === "MEDIUM") return "2"
+        if (name === "HIGH") return "3"
+
+        return "1"
+    }
+
+    // Render
+    return (
+        <>
+            <TextField
+                label="Task title"
+                className={styles.title}
+                variant="outlined"
+                value={title}
+                onChange={e => setTitle(e.target.value)}
+            />
+            <TextField
+                label="Task description"
+                className={styles.description}
+                rows={5}
+                multiline
+                value={description}
+                onChange={e => setDescription(e.target.value)}
+            />
+            <TextField
+                label="Gift and KPI for this task ;)"
+                className={styles.targets}
+                variant="outlined"
+                value={targets}
+                onChange={e => setTargets(e.target.value)}
+            />
+            <RadioGroup
+                row
+                aria-labelledby="Task priority"
+                className={styles.priority}
+                value={getPriorityNumberByName(priority)}
+                onChange={e => setPriority(getPriorityByNumber(e.target.value))}
+            >
+                <FormControlLabel value="1" control={<Radio />} label="Low" />
+                <FormControlLabel
+                    value="2"
+                    control={<Radio />}
+                    label="Medium"
+                />
+                <FormControlLabel value="3" control={<Radio />} label="High" />
+            </RadioGroup>
+            <Box className={styles.actions}>
+                <Button variant="outlined" color="error" onClick={handleClose}>
+                    cancel
+                </Button>
+                <Button variant="contained" onClick={handleAction}>
+                    Edit Task
+                </Button>
+            </Box>
+        </>
+    )
+}
+
+export default TaskForm
